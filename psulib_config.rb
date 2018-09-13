5ebbae6a0a99f545a7ad8df79376fae5f6cfd593
Ruth Kitchin Tillman <ruthtillman@gmail.com>
2018-09-13 10:37:56 -0400

    Narrowed down subject topic to LCSH
    For now, the subject topic FACET will just be based on LCSH. Adding note
    to ticket to add FAST to it as well.

--- psulib_config.rb
+++ psulib_config.rb
@@ -131,7 +131,7 @@ to_field 'subject_t', extract_marc(%W(
   653a:654abcde:655abc
 ).join(':'))
 to_field 'subject_addl_t', extract_marc("600vwxyz:610vwxyz:611vwxyz:630vwxyz:650vwxyz:651vwxyz:654vwxyz:655vwxyz")
-to_field 'subject_topic_facet', extract_marc("600abcdq:610ab:611ab:630ab:650a:653|*0|a", :trim_punctuation => true)
+to_field 'subject_topic_facet', extract_marc("600|*0|abcdq:610|*0|ab:611|*0|ab:630|*0|ab:650|*0|a:653|*0|a", :trim_punctuation => true)
 to_field 'subject_era_facet',  extract_marc("650y:651y:654y:655y", :trim_punctuation => true)
 to_field 'subject_geo_facet',  extract_marc("651a:650z",:trim_punctuation => true )
 
