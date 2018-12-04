686d1f96ec2f50f3d1b5ce370d5805db32811374
Banu Kutlu <bzk60@psu.edu>
2018-12-04 17:30:14 -0500

    more title specs

--- spec/lib/traject/title_spec.rb
+++ spec/lib/traject/title_spec.rb
@@ -10,14 +10,28 @@ RSpec.describe 'Title spec:' do
   let(:fixtures_doc) { File.new('./spec/fixtures/fixtures.mrc') }
   let(:results) { records.map { |rec| indexer.map_record(rec) }.to_a }
 
-  describe 'Vernacular titles' do
-    let(:result) { results.select { |r| r['id'] == ['2788022'] }.first }
-    it 'has a vernacular title as the title statement' do
+  describe 'Record with a vernacular title' do
+    subject(:result) { results.select { |r| r['id'] == ['2788022'] }.first }
+    it 'has the vernacular title as the title statement' do
       expect(result).to include('title_display_ssm' => ['小說ワンダフルライフ / 是枝裕和'])
+      expect(result['title_display_ssm'].length).to eq 1
     end
 
-    it 'has a latin title as the sub title' do
+    it 'has a latin title as the sub-title' do
       expect(result).to include('title_latin_display_ssm' => ['Shōsetsu wandafuru raifu / Koreeda Hirokazu'])
+      expect(result['title_latin_display_ssm'].length).to eq 1
+    end
+  end
+
+  describe 'Record with no vernacular title' do
+    subject(:result) { results.select { |r| r['id'] == ['2431513'] }.first }
+    it 'has the latin title as the title statement' do
+      expect(result).to include('title_display_ssm' => ['La ressemblance : suivi de la feintise, Jeff Edmunds / Jean Lahougue, Jeff Edmunds'])
+      expect(result['title_display_ssm'].length).to eq 1
+    end
+
+    it 'has no latin title as the sub-title' do
+      expect(result).not_to include('title_latin_display_ssm')
     end
   end
 end
