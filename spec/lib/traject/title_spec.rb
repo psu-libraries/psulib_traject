4e3afec0f96f4de400e7cc1aa199149a42e08c6b
Charlie Morris <cdm32@psu.edu>
2018-12-05 13:18:49 -0500

    This is technically unrelated to 42, rather it's the work of 60, but I merged in 60 because I wanted the testing framework for this bound withs work.
    * Changed fixtures.mrc to title_fixtures.mrc
    * Removed hash rockets

--- spec/lib/traject/title_spec.rb
+++ spec/lib/traject/title_spec.rb
@@ -6,19 +6,19 @@ RSpec.describe 'Title spec:' do
       i.load_config_file('./lib/traject/psulib_config.rb')
     end
   end
+  let(:fixtures_doc) { File.new('./spec/fixtures/title_fixtures.mrc') }
   let(:records) { MARC::Reader.new(fixtures_doc, external_encoding: 'UTF-8').to_a }
-  let(:fixtures_doc) { File.new('./spec/fixtures/fixtures.mrc') }
   let(:results) { records.map { |rec| indexer.map_record(rec) }.to_a }
 
   describe 'Record with a vernacular title' do
     subject(:result) { results.select { |r| r['id'] == ['2788022'] }.first }
     it 'has the vernacular title as the title statement' do
-      expect(result).to include('title_display_ssm' => ['小說ワンダフルライフ / 是枝裕和'])
+      expect(result).to include('title_display_ssm': '小說ワンダフルライフ / 是枝裕和')
       expect(result['title_display_ssm'].length).to eq 1
     end
 
     it 'has a latin title as the sub-title' do
-      expect(result).to include('title_latin_display_ssm' => ['Shōsetsu wandafuru raifu / Koreeda Hirokazu'])
+      expect(result).to include('title_latin_display_ssm': 'Shōsetsu wandafuru raifu / Koreeda Hirokazu')
       expect(result['title_latin_display_ssm'].length).to eq 1
     end
   end
@@ -26,7 +26,7 @@ RSpec.describe 'Title spec:' do
   describe 'Record with no vernacular title' do
     subject(:result) { results.select { |r| r['id'] == ['2431513'] }.first }
     it 'has the latin title as the title statement' do
-      expect(result).to include('title_display_ssm' => ['La ressemblance : suivi de la feintise, Jeff Edmunds / Jean Lahougue, Jeff Edmunds'])
+      expect(result).to include('title_display_ssm': 'La ressemblance : suivi de la feintise, Jeff Edmunds / Jean Lahougue, Jeff Edmunds')
       expect(result['title_display_ssm'].length).to eq 1
     end
 
