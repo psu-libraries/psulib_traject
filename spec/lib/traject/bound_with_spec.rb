badfe668e3d942f118361af291edb1dff7f5de57
Charlie Morris <cdm32@psu.edu>
2018-12-10 15:40:32 -0500

    rubocop'd

--- spec/lib/traject/bound_with_spec.rb
+++ spec/lib/traject/bound_with_spec.rb
@@ -10,16 +10,16 @@ RSpec.describe 'Bound with spec:' do
   end
 
   describe 'Child items bound in' do
-    let(:bound_with_catkey) { { '591' => { "ind1" => " ", "ind2" => " ", "subfields" => [ { 'a' => 'The high-caste Hindu woman / With introduction by Rachel L. Bodley' }, { 'c' =>'355035' } ] } } }
-    let(:bound_with_multi) { { '591' => { "ind1" => " ", "ind2" => " ", "subfields" => [ {'a' => 'blah blah blah'} ] } } }
+    let(:bound_with_catkey) { { '591' => { 'ind1' => ' ', 'ind2' => ' ', 'subfields' => [{ 'a' => 'The high-caste Hindu woman / With introduction by Rachel L. Bodley' }, { 'c' => '355035' }] } } }
+    let(:bound_with_multi) { { '591' => { 'ind1' => ' ', 'ind2' => ' ', 'subfields' => [{ 'a' => 'blah blah blah' }] } } }
     let(:bound_with_marc) { @indexer.map_record(MARC::Record.new_from_hash('fields' => [bound_with_catkey], 'leader' => leader)) }
     let(:bound_with_multi_marc) { @indexer.map_record(MARC::Record.new_from_hash('fields' => [bound_with_catkey, bound_with_multi], 'leader' => leader)) }
 
     it 'shows the parent title' do
-      expect(bound_with_marc['bound_with_title_struct']).to match ["{\"boundcatkey\": \"355035\", \"boundtitle\": \"The high-caste Hindu woman / With introduction by Rachel L. Bodley\"}"]
+      expect(bound_with_marc['bound_with_title_struct']).to match ['{"boundcatkey": "355035", "boundtitle": "The high-caste Hindu woman / With introduction by Rachel L. Bodley"}']
     end
     it 'shows the binding notes when there are more than one 591' do
-       expect(bound_with_multi_marc['bound_with_notes_ssm']).to include 'blah blah blah'
+      expect(bound_with_multi_marc['bound_with_notes_ssm']).to include 'blah blah blah'
     end
   end
 end
