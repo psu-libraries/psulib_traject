2b9616aacc4598efd1dd059c7ee7796b3e12ae5b
Charlie Morris <523381+cdmo@users.noreply.github.com>
2019-04-02 11:49:10 -0400

    Forgot to change name in spec

--- spec/lib/traject/macros/custom_spec.rb
+++ spec/lib/traject/macros/custom_spec.rb
@@ -139,32 +139,32 @@ RSpec.describe 'Macros spec:' do
       @empty_record = MARC::Record.new
       @empty_record.append(MARC::ControlField.new('001', '000000000'))
       result = @indexer.map_record(@empty_record)
-      expect(result['access_facet_ssim']).to be_nil
+      expect(result['access_facet']).to be_nil
     end
 
     it 'produces In the Library and Online when a record has both an online copy and a physical copy' do
       result = @indexer.map_record(MARC::Reader.new(File.join(fixture_path, 'access_in_library.mrc')).to_a.first)
-      expect(result['access_facet_ssim']).to contain_exactly 'In the Library', 'Online'
+      expect(result['access_facet']).to contain_exactly 'In the Library', 'Online'
     end
 
     it 'produces On Order when a record has a copy with an on-order location' do
       result = @indexer.map_record(MARC::Reader.new(File.join(fixture_path, 'access_on_order.mrc')).to_a.first)
-      expect(result['access_facet_ssim']).to contain_exactly 'On Order'
+      expect(result['access_facet']).to contain_exactly 'On Order'
     end
 
     it 'produces In the Library and Online when a record has an online copy, a physical copy and an on-order copy' do
       result = @indexer.map_record(MARC::Reader.new(File.join(fixture_path, 'access_all.mrc')).to_a.first)
-      expect(result['access_facet_ssim']).to contain_exactly 'In the Library', 'Online'
+      expect(result['access_facet']).to contain_exactly 'In the Library', 'Online'
     end
 
     it 'produces Other when a record has a 949m library code that is not listed' do
       result = @indexer.map_record(MARC::Reader.new(File.join(fixture_path, 'access_other.mrc')).to_a.first)
-      expect(result['access_facet_ssim']).to contain_exactly 'Other'
+      expect(result['access_facet']).to contain_exactly 'Other'
     end
 
     it 'empty when a record has a 949m library code ZREMOVED or XTERNAL' do
       result = @indexer.map_record(MARC::Reader.new(File.join(fixture_path, 'access_zremoved.mrc')).to_a.first)
-      expect(result['access_facet_ssim']).to be_nil
+      expect(result['access_facet']).to be_nil
     end
   end
 end
