982f3a0ef160e7443df3e6ef1823aae6160862ed
Charlie Morris <523381+cdmo@users.noreply.github.com>
2019-05-15 09:49:44 -0400

    * Adds ability to specify qa or prod (#150)
    * Adds ability to specify qa or prod
    * Adds ability to specify hourly or daily

--- config/schedule.rb
+++ config/schedule.rb
@@ -3,8 +3,14 @@
 set :environment_variable, 'RUBY_ENVIRONMENT'
 env 'MAILTO', 'cdm32@psu.edu'
 
-# Process incrementals (adds/deletes from Symphony)
+# Process incrementals (adds/deletes from Symphony) daily
 every :weekday, at: '04:10am' do
-  rake 'incrementals:import_daily'
-  rake 'incrementals:delete_daily'
+  rake "incrementals:import['daily']"
+  rake "incrementals:delete['daily']"
+end
+
+# Process incrementals (adds/deletes from Symphony) hourly (3 minutes past the hour everyday between 7am and midnight)
+every '3 0,7-23 * * *' do
+  rake "incrementals:import['hourly']"
+  rake "incrementals:delete['hourly']"
 end
