28bb3bfca3f7a941b5eaf9b5f7ff6af72144bb1b
Banu Hapeloglu Kutlu <bzk60@psu.edu>
2019-06-26 15:08:40 -0500

    Update spec/lib/traject/macros/custom_spec.rb
    Co-Authored-By: Charlie Morris <523381+cdmo@users.noreply.github.com>

--- spec/lib/traject/macros/custom_spec.rb
+++ spec/lib/traject/macros/custom_spec.rb
@@ -128,7 +128,7 @@ RSpec.describe 'Macros spec:' do
       end
     end
 
-    context 'when 035 field includes OCoLC' do
+    context 'when 035 field includes \"OCoLC\"' do
       it 'maps the oclc number' do
         expect(result['oclc_number_display_ssm']).to include('154806744')
       end
