8e9170d76bf0966c87f8b47d1d32f89ae6e22786
Dann Bohn <djb44@psu.edu>
2020-10-06 14:53:12 -0400

    comment letting user know whenever schedule should match splunk schedule

--- config/schedule.rb
+++ config/schedule.rb
@@ -3,12 +3,14 @@
 set :environment_variable, 'RUBY_ENVIRONMENT'
 
 # Process incrementals (adds/deletes from Symphony) daily
+# Changes to this schedule should be reflected in the "Traject Incrementals (daily)" alert in splunk
 every :day, at: '04:10am' do
   rake "incrementals:import['daily']"
   rake "incrementals:delete['daily']"
 end
 
 # Process incrementals (adds/deletes from Symphony) hourly (3 minutes past the hour everyday between 7am and midnight)
+# Changes to this schedule should be reflected in the "Traject Incrementals" alert in splunk
 every '3 0,7-23 * * *' do
   rake "incrementals:import['hourly']"
   rake "incrementals:delete['hourly']"
