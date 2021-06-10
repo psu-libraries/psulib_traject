6f8f9051f836980ec0d978b40561fbb5abaeb162
whereismyjetpack <djb44@psu.edu>
2021-06-10 17:36:36 -0400

    adds basic auth, and ENV config support to solr_manager

--- lib/psulib_traject/solr_manager.rb
+++ lib/psulib_traject/solr_manager.rb
@@ -13,6 +13,8 @@ module PsulibTraject
       Config.setup do |config|
         config.const_name = 'ConfigSettings'
         config.use_env = true
+        config.env_prefix = 'SETTINGS'
+        config.env_separator = '__'
         config.load_and_set_settings(Config.setting_files('config', ENV['RUBY_ENVIRONMENT']))
       end
     end
@@ -33,11 +35,19 @@ module PsulibTraject
       end
 
       def connection
-        @connection ||= Faraday.new(ConfigSettings.solr.url) do |faraday|
+        @connection ||= Faraday.new(url) do |faraday|
+          if ConfigSettings.solr.username && ConfigSettings.solr.password
+            faraday.request :basic_auth, ConfigSettings.solr.username, ConfigSettings.solr.password
+          end
           faraday.request :multipart
           faraday.adapter :net_http
         end
       end
+
+      def url
+        "#{ConfigSettings.solr.protocol}://#{ConfigSettings.solr.host}:#{ConfigSettings.solr.port}/solr"
+      end
+
   end
 
   # Lightweight Solr collection abstraction
