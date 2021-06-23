384fe3536d3c77d1ae942212140681e259699dad
Adam Wead <awead@users.noreply.github.com>
2021-06-23 11:01:25 -0400

    Configure Solr's port (#304)
    This allows us to run Solr on a different port, which is helpful if you
    have multiple apps running with their own Solr, or want to test
    different versions on different ports.

--- spec/lib/psulib_traject/solr_manager_spec.rb
+++ spec/lib/psulib_traject/solr_manager_spec.rb
@@ -6,10 +6,16 @@ require 'config'
 RSpec.describe PsulibTraject::SolrManager do
   subject(:solr_manager) { described_class.new }
 
+  let(:port) { ConfigSettings.solr.port || '8983' }
+
   describe '#last_incremented_collection' do
     before do
-      stub_request(:get, 'http://localhost:8983/solr/admin/collections?action=LIST')
-        .to_return(status: 200, body: '{"responseHeader":{"status":0,"QTime":0},"collections":["psul_catalog_v1","psul_catalog_v2"]}', headers: {})
+      stub_request(:get, "http://localhost:#{port}/solr/admin/collections?action=LIST")
+        .to_return(
+          status: 200,
+          body: '{"responseHeader":{"status":0,"QTime":0},"collections":["psul_catalog_v1","psul_catalog_v2"]}',
+          headers: {}
+        )
     end
 
     it 'emits the last incremented collection' do
