ef6658d1425ebfec62db59e95321af5d13e202e9
Banu Kutlu <bzk60@psu.edu>
2021-07-21 15:51:24 -0400

    lc specs

--- lib/psulib_traject/call_numbers/call_number_base.rb
+++ lib/psulib_traject/call_numbers/call_number_base.rb
@@ -10,15 +10,27 @@ module PsulibTraject::CallNumbers
       'map folder', 'mfilm', 'mfiche', 'os box', 'os folder', 'pl', 'reel',  'series',
       'sheet', 'slides', 'small folder', 'small map folder', 'suppl', 'text', 'tube'
     ].freeze
-    ADDL_VOL_PATTERN = /[:\/]?(#{ADDL_VOL_PARTS.join('|')}).*/i.freeze
-    VOL_PATTERN         = /([.:\/(])?(n\.s\.?,? ?)?[:\/]?(#{VOL_PARTS}|#{MONTHS})[. -\/]?\d+([\/-]\d+)?( \d{4}([\/-]\d{4})?)?( ?suppl\.?)?/i.freeze
-    VOL_PATTERN_LOOSER  = /([.:\/(])?(n\.s\.?,? ?)?[:\/]?(#{VOL_PARTS}|#{MONTHS})[. -]?\d+.*/i.freeze
-    VOL_PATTERN_LETTERS = /([.:\/(])?(n\.s\.?,? ?)?[:\/]?(#{VOL_PARTS}|#{MONTHS})[\/. -]?[A-Z]?([\/-][A-Z]+)?.*/i.freeze
-    FOUR_DIGIT_YEAR_REGEX = /\W *(20|19|18|17|16|15|14)\d{2}\D?$?/.freeze
-    LOOSE_MONTHS_REGEX = /([.:\/(])? *#{MONTHS}/i.freeze
+    ADDL_VOL_PATTERN = /[\:\/]?(#{ADDL_VOL_PARTS.join('|')}).*/i.freeze
+    VOL_PATTERN         = /([\.:\/\(])?(n\.s\.?\,? ?)?[\:\/]?(#{VOL_PARTS}|#{MONTHS})[\. -\/]?\d+([\/-]\d+)?( \d{4}([\/-]\d{4})?)?( ?suppl\.?)?/i
+    VOL_PATTERN_LOOSER  = /([\.:\/\(])?(n\.s\.?\,? ?)?[\:\/]?(#{VOL_PARTS}|#{MONTHS})[\. -]?\d+.*/i
+    VOL_PATTERN_LETTERS = /([\.:\/\(])?(n\.s\.?\,? ?)?[\:\/]?(#{VOL_PARTS}|#{MONTHS})[\/\. -]?[A-Z]?([\/-][A-Z]+)?.*/i
+    FOUR_DIGIT_YEAR_REGEX = /\W *(20|19|18|17|16|15|14)\d{2}\D?$?/
+    LOOSE_MONTHS_REGEX = /([\.:\/\(])? *#{MONTHS}/i
+
 
     def lopped
       raise NotImplementedError
+    end
+
+    class << self
+      def lop_years(value)
+        month_b4_year = value[0...(value.index(LOOSE_MONTHS_REGEX) || value.length)]
+        year_b4_month = value[0...(value.index(FOUR_DIGIT_YEAR_REGEX) || value.length)]
+        shortest_lopped = [month_b4_year, year_b4_month].min_by(&:length)
+        return value if shortest_lopped.length < 4
+
+        shortest_lopped
+      end
     end
   end
 end
