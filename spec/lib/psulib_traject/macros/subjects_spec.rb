d46a8fb7642e5cc86547e7c40eb2c7e85c5c25b7
Adam Wead <awead@users.noreply.github.com>
2021-08-26 14:46:51 -0400

    Refactor subjects macro test to use MarcBot

--- spec/lib/psulib_traject/macros/subjects_spec.rb
+++ spec/lib/psulib_traject/macros/subjects_spec.rb
@@ -1,33 +1,65 @@
 # frozen_string_literal: true
 
 RSpec.describe PsulibTraject::Macros::Subjects do
+  let(:result) { indexer.map_record(record) }
+
   describe 'process_subject_hierarchy' do
-    before(:all) do
-      @subject610 = { '610' => { 'ind1' => '', 'ind2' => '5', 'subfields' => [{ 'a' => 'Include' }] } }
-      @subject600 = { '600' => { 'ind1' => '', 'ind2' => '0', 'subfields' => [{ 'a' => 'John.' }, { 't' => 'Title.' }, { 'v' => 'split genre' }, { 'd' => '2015' }, { '2' => 'special' }] } }
-      @subject630 = { '630' => { 'ind1' => '', 'ind2' => '0', 'subfields' => [{ 'x' => 'Fiction' }, { 'y' => '1492' }, { 'z' => "don't ignore" }, { 't' => 'TITLE.' }] } }
-      @sample_marc = MARC::Record.new_from_hash('fields' => [@subject610, @subject600, @subject630], 'leader' => '1234567890')
+    let(:record) do
+      MarcBot.build(
+        :record,
+        f610: {
+          indicator2: '5',
+          a: 'Include'
+        },
+        f600: {
+          indicator2: '0',
+          a: 'John.',
+          t: 'Title.',
+          v: 'split genre',
+          d: '2015',
+          2 => 'special'
+        },
+        f630: {
+          indicator2: '0',
+          x: 'Fiction',
+          y: '1492',
+          z: "don't ignore",
+          t: 'TITLE.'
+        }
+      )
     end
 
     it 'only separates v,x,y,z with em dash, strips punctuation' do
-      result = indexer.map_record(@sample_marc)
-      expect(result['subject_display_ssm']).to include('Include')
-      expect(result['subject_display_ssm']).to include("John. Title#{described_class::SEPARATOR}split genre 2015")
-      expect(result['subject_display_ssm']).to include("Fiction#{described_class::SEPARATOR}1492#{described_class::SEPARATOR}don't ignore TITLE")
+      expect(result['subject_display_ssm']).to include(
+        'Include',
+        "John. Title#{described_class::SEPARATOR}split genre 2015",
+        "Fiction#{described_class::SEPARATOR}1492#{described_class::SEPARATOR}don't ignore TITLE"
+      )
     end
   end
 
   describe 'process_subject_topic_facet' do
-    before(:all) do
-      @subject600 = { '600' => { 'ind1' => '', 'ind2' => '0', 'subfields' => [{ 'a' => 'John.' }, { 'x' => 'Join' }, { 't' => 'Title' }, { 'd' => '2015.' }] } }
-      @subject650 = { '650' => { 'ind1' => '', 'ind2' => '0', 'subfields' => [{ 'x' => 'Fiction' }, { 'y' => '1492' }, { 'v' => 'split genre' }] } }
-      @sample_marc = MARC::Record.new_from_hash('fields' => [@subject600, @subject650], 'leader' => '1234567890')
+    let(:record) do
+      MarcBot.build(
+        :record,
+        f600: {
+          indicator2: '0',
+          a: 'John.',
+          x: 'Join',
+          t: 'Title',
+          d: '2015.'
+        },
+        f650: {
+          indicator2: '0',
+          x: 'Fiction',
+          y: '1492',
+          v: 'split genre'
+        }
+      )
     end
 
     it 'includes subjects split along v, x, y and z, strips punctuation' do
-      result = indexer.map_record(@sample_marc)
-      expect(result['subject_topic_facet']).to include('John. Title 2015')
-      expect(result['subject_topic_facet']).to include('Fiction')
+      expect(result['subject_topic_facet']).to include('John. Title 2015', 'Fiction')
     end
   end
 end
