dfaed7bb459066695cae61348cfa996eff7056b9
Banu Kutlu <bzk60@psu.edu>
2021-09-02 17:00:09 -0400

    Building off of Stanford's traject repository, determines a base call number for Dewey classifications.
    Using the Lcsort gem, creates shelf keys for dewey numbers to sort
    in forward and reverse directions.

--- lib/psulib_traject/processors/call_number/dewey.rb
+++ lib/psulib_traject/processors/call_number/dewey.rb
@@ -2,20 +2,31 @@
 
 module PsulibTraject::Processors::CallNumber
   class Dewey < Base
-    attr_reader :call_number,
-                :serial
-
     def initialize(call_number, serial: false)
+      match_data = /
+        (?<klass_number>\d{1,3})(?<klass_decimal>\.?\d+)?\s*
+        (?<doon1>(\d{1,4})(?:ST|ND|RD|TH|D)?\s+)?\s*
+        (?<cutter1>[.\/]?[a-zA-Z]+\d+([a-zA-Z]*(?![0-9])))?\s*
+        (?<removeables>(?<doon2>(\d{1,4})(?:ST|ND|RD|TH|D)?\s+)?\s*
+        (?<cutter2>[.\/]?[a-zA-Z]+\d+([a-zA-Z]*(?![0-9])))?\s*
+        (?<doon3>(\d{1,4})(?:ST|ND|RD|TH|D)?\s+)?\s*
+        (?<cutter3>[.\/]?[a-zA-Z]+\d+([a-zA-Z]*(?![0-9])))?\s*
+        (?<rest>.*))
+      /x.match(call_number)
+
       @call_number = call_number
+      match_data ||= {}
+      @klass_number = match_data[:klass_number]
+      @klass_decimal = match_data[:klass_decimal]
+      @doon1 = match_data[:doon1]
+      @cutter1 = match_data[:cutter1]
+      @doon2 = match_data[:doon2]
+      @doon3 = match_data[:doon3]
+      @cutter2 = match_data[:cutter2]
+      @cutter3 = match_data[:cutter3]
+      @rest = match_data[:rest]
+      @removeables = match_data[:removeables]
       @serial = serial
-    end
-
-    def scheme
-      'dewey'
-    end
-
-    def reduce
-      call_number
     end
   end
 end
--- spec/lib/psulib_traject/holdings_spec.rb
+++ spec/lib/psulib_traject/holdings_spec.rb
@@ -15,17 +15,18 @@ RSpec.describe PsulibTraject::Holdings do
   end
   let(:context) { instance_spy('Traject::Indexer::Context', output_hash: {}) }
 
-  let(:mocked_lc_holding) { instance_spy(PsulibTraject::Processors::CallNumber::LC, reduce: 'LC Call Number') }
-  let(:mocked_lcper_holding) { instance_spy(PsulibTraject::Processors::CallNumber::LC, reduce: 'LCPER Call Number') }
-  let(:mocked_dewey_holding) { instance_spy(PsulibTraject::Processors::CallNumber::Dewey, reduce: 'Dewey Call Number') }
+  let(:mocked_lc_holding) { instance_spy(PsulibTraject::Processors::CallNumber::LC, reduce: 'AB123 .C456 2000 LC Call Number') }
+  let(:mocked_lcper_holding) { instance_spy(PsulibTraject::Processors::CallNumber::LC, reduce: 'AB123 .C456 2000 LCPER Call Number') }
+  let(:mocked_dewey_holding) { instance_spy(PsulibTraject::Processors::CallNumber::Dewey, reduce: '123.1A23a Dewey Call Number') }
   let(:mocked_other_holding) { instance_spy(PsulibTraject::Processors::CallNumber::Other, reduce: 'ASIS Call Number') }
 
   before do
-    allow(PsulibTraject::Processors::CallNumber::LC).to receive(:new).with('LC Call Number', serial: false)
+    allow(PsulibTraject::Processors::CallNumber::LC).to receive(:new).with('AB123 .C456 2000 LC Call Number', serial: false)
       .and_return(mocked_lc_holding)
-    allow(PsulibTraject::Processors::CallNumber::LC).to receive(:new).with('LCPER Call Number', serial: false)
+    allow(PsulibTraject::Processors::CallNumber::LC).to receive(:new).with('AB123 .C456 2000 LCPER Call Number', serial: false)
       .and_return(mocked_lcper_holding)
-    allow(PsulibTraject::Processors::CallNumber::Dewey).to receive(:new).with('Dewey Call Number').and_return(mocked_dewey_holding)
+    allow(PsulibTraject::Processors::CallNumber::Dewey).to receive(:new).with('123.1A23a Dewey Call Number', serial: false)
+      .and_return(mocked_dewey_holding)
     allow(PsulibTraject::Processors::CallNumber::Other).to receive(:new).with('ASIS Call Number').and_return(mocked_other_holding)
   end
 
@@ -59,68 +60,63 @@ RSpec.describe PsulibTraject::Holdings do
       it { is_expected.to be_empty }
     end
 
+    context 'with a record that has non browsable lc call number' do
+      let(:fields) { [MARC::DataField.new('949', '', '', ['a', 'Some Call Number'], ['w', 'LC'])] }
+
+      it { is_expected.to be_empty }
+    end
+
+    context 'with a record that has non browsable dewey call number' do
+      let(:fields) { [MARC::DataField.new('949', '', '', ['a', 'Microfilm E243 June 1-15 2021'], ['w', 'DEWEY'])] }
+
+      it { is_expected.to be_empty }
+    end
+
     context 'with a record that has two holdings with same call number' do
       let(:fields) do
         [
-          MARC::DataField.new('949', '', '', ['a', 'LC Call Number'], ['w', 'LC'], ['l', 'Location']),
-          MARC::DataField.new('949', '', '', ['a', 'LC Call Number'], ['w', 'LC'], ['l', 'Location'])
+          MARC::DataField.new('949', '', '', ['a', 'AB123 .C456 2000 LC Call Number'], ['w', 'LC'], ['l', 'Location']),
+          MARC::DataField.new('949', '', '', ['a', 'AB123 .C456 2000 LC Call Number'], ['w', 'LC'], ['l', 'Location'])
         ]
       end
 
-      it { is_expected.to contain_exactly 'LC Call Number' }
+      it { is_expected.to contain_exactly 'AB123 .C456 2000 LC Call Number' }
     end
 
     context 'with a record that has holdings with different classifications' do
       let(:fields) do
         [
-          MARC::DataField.new('949', '', '', ['a', 'LC Call Number'], ['w', 'LC'], ['l', 'Location']),
-          MARC::DataField.new('949', '', '', ['a', 'LCPER Call Number'], ['w', 'LCPER'], ['l', 'Location']),
-          MARC::DataField.new('949', '', '', ['a', 'Dewey Call Number'], ['w', 'DEWEY'], ['l', 'Location']),
+          MARC::DataField.new('949', '', '', ['a', 'AB123 .C456 2000 LC Call Number'], ['w', 'LC'], ['l', 'Location']),
+          MARC::DataField.new('949', '', '', ['a', 'AB123 .C456 2000 LCPER Call Number'], ['w', 'LCPER'], ['l', 'Location']),
+          MARC::DataField.new('949', '', '', ['a', '123.1A23a Dewey Call Number'], ['w', 'DEWEY'], ['l', 'Location']),
           MARC::DataField.new('949', '', '', ['a', 'ASIS Call Number'], ['w', 'ASIS'], ['l', 'Location'])
         ]
       end
 
-      it { is_expected.to contain_exactly 'ASIS Call Number', 'Dewey Call Number', 'LC Call Number', 'LCPER Call Number' }
-    end
-
-    context 'when requesting only LC and LCPER classifications' do
-      subject(:holdings) do
-        described_class.call(record: record, context: context, classification: ['LC', 'LCPER']).map(&:value)
-      end
-
-      let(:fields) do
-        [
-          MARC::DataField.new('949', '', '', ['a', 'LC Call Number'], ['w', 'LC'], ['l', 'Location']),
-          MARC::DataField.new('949', '', '', ['a', 'LCPER Call Number'], ['w', 'LCPER'], ['l', 'Location']),
-          MARC::DataField.new('949', '', '', ['a', 'Dewey Call Number'], ['w', 'DEWEY'], ['l', 'Location']),
-          MARC::DataField.new('949', '', '', ['a', 'ASIS Call Number'], ['w', 'ASIS'], ['l', 'Location'])
-        ]
-      end
-
-      it { is_expected.to contain_exactly 'LC Call Number', 'LCPER Call Number' }
+      it { is_expected.to contain_exactly '123.1A23a Dewey Call Number', 'AB123 .C456 2000 LC Call Number', 'AB123 .C456 2000 LCPER Call Number' }
     end
 
     context 'with a record that has holdings with identical base call numbers' do
       let(:fields) do
         [
-          MARC::DataField.new('949', '', '', ['a', 'LC Call Number v.1'], ['w', 'LC'], ['l', 'Location']),
-          MARC::DataField.new('949', '', '', ['a', 'LC Call Number v.2'], ['w', 'LC'], ['l', 'Location'])
+          MARC::DataField.new('949', '', '', ['a', 'AB123 .C456 2000 LC Call Number v.1'], ['w', 'LC'], ['l', 'Location']),
+          MARC::DataField.new('949', '', '', ['a', 'AB123 .C456 2000 LC Call Number v.2'], ['w', 'LC'], ['l', 'Location'])
         ]
       end
 
-      let(:mocked_lc_holding_1) { instance_spy(PsulibTraject::Processors::CallNumber::LC, reduce: 'LC Call Number v.1') }
-      let(:mocked_lc_holding_2) { instance_spy(PsulibTraject::Processors::CallNumber::LC, reduce: 'LC Call Number v.2') }
+      let(:mocked_lc_holding_1) { instance_spy(PsulibTraject::Processors::CallNumber::LC, reduce: 'AB123 .C456 2000 LC Call Number v.1') }
+      let(:mocked_lc_holding_2) { instance_spy(PsulibTraject::Processors::CallNumber::LC, reduce: 'AB123 .C456 2000 LC Call Number v.2') }
 
       before do
-        allow(PsulibTraject::Processors::CallNumber::LC).to receive(:new).with('LC Call Number v.1', serial: false)
+        allow(PsulibTraject::Processors::CallNumber::LC).to receive(:new).with('AB123 .C456 2000 LC Call Number v.1', serial: false)
           .and_return(mocked_lc_holding_1)
-        allow(PsulibTraject::Processors::CallNumber::LC).to receive(:new).with('LC Call Number v.2', serial: false)
+        allow(PsulibTraject::Processors::CallNumber::LC).to receive(:new).with('AB123 .C456 2000 LC Call Number v.2', serial: false)
           .and_return(mocked_lc_holding_2)
-        allow(mocked_lc_holding_1).to receive(:reduce).and_return('LC Call Number')
-        allow(mocked_lc_holding_2).to receive(:reduce).and_return('LC Call Number')
+        allow(mocked_lc_holding_1).to receive(:reduce).and_return('AB123 .C456 2000 LC Call Number')
+        allow(mocked_lc_holding_2).to receive(:reduce).and_return('AB123 .C456 2000 LC Call Number')
       end
 
-      it { is_expected.to contain_exactly 'LC Call Number' }
+      it { is_expected.to contain_exactly 'AB123 .C456 2000 LC Call Number' }
     end
   end
 end
