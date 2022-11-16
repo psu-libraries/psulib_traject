a6f5070d89806990cf44447e5ee40a4e09fa41bd
Alex Kiessling <ajkiessl@gmail.com>
2022-11-16 11:37:13 -0500

    Test for new allowed genre indicators and separating internal genres by a hyphen.

--- spec/integration/macros_spec.rb
+++ spec/integration/macros_spec.rb
@@ -256,11 +256,21 @@ RSpec.describe 'Macros' do
     let(:genre655_fast) { { '655' => { 'ind1' => '', 'ind2' => '7', 'subfields' => [{ 'a' => 'Fiction films' }, { 'b' => '1900' }, { '2' => 'fast' }, { 'z' => 'Germany' }] } } }
     let(:genre655_lcgft) { { '655' => { 'ind1' => '', 'ind2' => '7', 'subfields' => [{ 'a' => 'Drama.' }, { '2' => 'lcgft' }] } } }
     let(:genre655_aat) { { '655' => { 'ind1' => '', 'ind2' => '7', 'subfields' => [{ 'a' => 'Novels' }, { '2' => 'aat' }] } } }
-    let(:result) { indexer.map_record(MARC::Record.new_from_hash('fields' => [genre650, genre655_fast, genre655_lcgft, genre655_aat], 'leader' => leader)) }
+    let(:genre655_bogus) { { '655' => { 'ind1' => '', 'ind2' => '7', 'subfields' => [{ 'a' => 'Bogus' }, { '2' => 'bogus' }] } } }
+    let(:genre655_rbgenr) { { '655' => { 'ind1' => '', 'ind2' => '7', 'subfields' => [{ 'a' => 'Periodicals' }, { '2' => 'rbgenr' }] } } }
+    let(:genre655_rbbin) { { '655' => { 'ind1' => '', 'ind2' => '7', 'subfields' => [{ 'a' => 'Rbbin' }, { '2' => 'rbbin' }] } } }
+    let(:genre655_rbprov) { { '655' => { 'ind1' => '', 'ind2' => '7', 'subfields' => [{ 'a' => 'Rbprov' }, { '2' => 'rbprov' }] } } }
+    let(:genre655_rbpub) { { '655' => { 'ind1' => '', 'ind2' => '7', 'subfields' => [{ 'a' => 'Rbpub' }, { '2' => 'rbpub' }] } } }
+    let(:genre655_rbpri) { { '655' => { 'ind1' => '', 'ind2' => '7', 'subfields' => [{ 'a' => 'Rbpri' }, { '2' => 'rbpri' }] } } }
+    let(:genre655_rbmscv) { { '655' => { 'ind1' => '', 'ind2' => '7', 'subfields' => [{ 'a' => 'Rbmscv' }, { '2' => 'rbmscv' }] } } }
+    let(:result) { indexer.map_record(MARC::Record.new_from_hash('fields' => [genre650, genre655_fast, genre655_lcgft, 
+                                                                              genre655_aat, genre655_bogus, genre655_rbgenr, 
+                                                                              genre655_rbbin, genre655_rbprov, genre655_rbpub,
+                                                                              genre655_rbpri, genre655_rbmscv], 'leader' => leader)) }
 
-    it 'limits 655 to fast and lcgft genres' do
-      expect(result['genre_display_ssm']).to include('Fiction films 1900 Germany')
-      expect(result['genre_display_ssm']).to include('Drama')
+    it 'limits 655 genres and separates multiple internal genres with a hyphen' do
+      expect(result['genre_display_ssm']).to eq(["Fiction films - 1900 - Germany", "Drama", "Novels", 
+                                                 "Periodicals", "Rbbin", "Rbprov", "Rbpub", "Rbpri", "Rbmscv"])
     end
   end
 
