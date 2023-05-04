77c257022a78057423b8ee01c0dcc01c8e0bf5e9
Alex Kiessling <ajkiessl@gmail.com>
2023-05-04 14:02:41 -0400

    Rubocop fixes

--- lib/psulib_traject/update_locations.rb
+++ lib/psulib_traject/update_locations.rb
@@ -1,35 +1,40 @@
-require 'open-uri'
+# frozen_string_literal: true
+
+require 'net/http'
 require 'json'
 
 module UpdateLocations
-  def self.call
-    File.open("./lib/translation_maps/locations.properties", "w") do |file|
+  module_function
+
+  def call
+    File.open('./lib/translation_maps/locations.properties', 'w') do |file|
       file.truncate(0)
-      open(url, headers) do |response|
-        json = JSON.parse(response.read.to_s)
-        json_sorted = json.sort_by{|i| i["fields"]["displayName"] }
-        json_sorted_uniqd = json_sorted.uniq{|i| i["fields"]["displayName"] }
-        json_sorted_uniqd.each do |line|
-          file.write "#{line["fields"]["displayName"]} = #{line["fields"]["translatedDescription"]}\n"
-        end
+      json = JSON.parse(response.to_s)
+      json_sorted = json.sort_by { |i| i['fields']['displayName'] }
+      json_sorted_uniqd = json_sorted.uniq { |i| i['fields']['displayName'] }
+      json_sorted_uniqd.each do |line|
+        file.write "#{line['fields']['displayName']} = #{line['fields']['translatedDescription']}\n"
       end
     end
   end
 
-  private
+  def response
+    http = Net::HTTP.new(url.host, url.port)
+    http.use_ssl = (url.scheme == 'https')
+    request = Net::HTTP::Get.new(url, headers)
+    http.request(request).body
+  end
 
-    def self.headers
-      {
-        'Accept' => 'application/json',
-        'Content-Type' => 'application/json',
-        'sd-originating-app-id' => 'DHCTemplate',
-        'x-sirs-clientID' => 'PSUCATALOG'
-      }
-    end
+  def headers
+    {
+      'Accept' => 'application/json',
+      'Content-Type' => 'application/json',
+      'sd-originating-app-id' => 'DHCTemplate',
+      'x-sirs-clientID' => 'PSUCATALOG'
+    }
+  end
 
-    def self.url
-      'https://cat.libraries.psu.edu:28443/symwsbc/policy/location/simpleQuery?key=*&includeFields=displayName%2Cdescription%2CtranslatedDescription'
-    end
+  def url
+    URI.parse('https://cat.libraries.psu.edu:28443/symwsbc/policy/location/simpleQuery?key=*&includeFields=displayName,description,translatedDescription')
+  end
 end
-
-UpdateLocations.call
