3ccdf12eb8c7da76f1e8c1637e49c55494cdafe0
Alex Kiessling <ajkiessl@gmail.com>
2023-05-18 11:23:25 -0400

    Fixes and adds test

--- spec/integration/macros_spec.rb
+++ spec/integration/macros_spec.rb
@@ -213,7 +213,7 @@ RSpec.describe 'Macros' do
 
     context 'when a record does not have a PSU thesis code' do
       let(:code_949) do
-        { '949' => { 'subfields' => [{ 'a' => 'Thesis 2011mOrr,A', 't' => 'THESIS-A' }] } }
+        { '949' => { 'subfields' => [{ 'a' => 'Thesis 2011mOrr,A', 't' => 'THESIS-B' }] } }
       end
 
       let(:result) { indexer.map_record(MARC::Record.new_from_hash('fields' => [thesis_dept_699, code_949], 'leader' => leader)) }
@@ -224,14 +224,28 @@ RSpec.describe 'Macros' do
     end
 
     context 'when a record does have a PSU thesis code' do
-      let(:code_949) do
-        { '949' => { 'subfields' => [{ 'a' => 'Thesis 2011mOrr,A', 't' => 'THESIS-B' }] } }
+      context 'when thesis code is THESIS-D' do
+        let(:code_949) do
+          { '949' => { 'subfields' => [{ 'a' => 'Thesis 2011mOrr,A', 't' => 'THESIS-D' }] } }
+        end
+
+        let(:result) { indexer.map_record(MARC::Record.new_from_hash('fields' => [thesis_dept_699, code_949], 'leader' => leader)) }
+
+        it 'indexes the theses department data' do
+          expect(result['thesis_dept_facet']).to eq ['Acoustics']
+        end
       end
 
-      let(:result) { indexer.map_record(MARC::Record.new_from_hash('fields' => [thesis_dept_699, code_949], 'leader' => leader)) }
+      context 'when thesis code is THESIS-M' do
+        let(:code_949) do
+          { '949' => { 'subfields' => [{ 'a' => 'Thesis 2011mOrr,A', 't' => 'THESIS-M' }] } }
+        end
 
-      it 'indexes the theses department data' do
-        expect(result['thesis_dept_facet']).to eq ['Acoustics']
+        let(:result) { indexer.map_record(MARC::Record.new_from_hash('fields' => [thesis_dept_699, code_949], 'leader' => leader)) }
+
+        it 'indexes the theses department data' do
+          expect(result['thesis_dept_facet']).to eq ['Acoustics']
+        end
       end
     end
   end
